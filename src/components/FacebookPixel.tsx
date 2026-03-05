4a1029fa01491d90f243be43a56235a4b472ae42
Itallo Aquino <italloaquino.dev@gmail.com>
2026-03-05 18:21:33 +0000

    fix: move facebook pixel to react component to fix build errors

---
+++ src/components/FacebookPixel.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+
+const FacebookPixel = () => {
+    useEffect(() => {
+        const PIXEL_ID = "907951455286362";
+
+        // Initialize Facebook Pixel if not already initialized
+        if (!(window as any).fbq) {
+            (function (f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
+                if (f.fbq) return;
+                n = f.fbq = function () {
+                    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = "2.0";
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+            })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js", undefined, undefined, undefined);
+            (window as any).fbq("init", PIXEL_ID);
+        }
+        (window as any).fbq("track", "PageView");
+    }, []);
+
+    return null;
+};
+
+export default FacebookPixel;
